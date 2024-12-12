5b00fde59130faf4193ea55e61fcf1d7deb48fac
madhura-avaloq <madhura.khandare@avaloq.com>
2024-12-12 12:32:24 +0530

    commit

--- boostrapfrontend/src/index.js
+++ boostrapfrontend/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Your custom CSS
-import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+//import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
